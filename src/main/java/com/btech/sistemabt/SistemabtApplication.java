6195608d9181c6f508ba5fa6c343b82ad6d076b9
Kori55 <vina@duoc.acad>
2025-05-07 14:35:02 -0400

    a

---
+++ src/main/java/com/btech/sistemabt/SistemabtApplication.java
@@ -0,0 +1,13 @@
+package com.btech.sistemabt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemabtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemabtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/btech/sistemabt/SistemabtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btech.sistemabt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemabtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
